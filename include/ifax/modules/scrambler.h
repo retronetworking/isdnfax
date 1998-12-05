d97307490903cd730d649fa934bfc9084e939e37
Morten Rolland <Morten.Rolland@asker.mail.telia.com>
1998-12-05 16:43:02 +0000

    Needs to be able to command scrambler to initialize.

---
+++ include/ifax/modules/scrambler.h
@@ -0,0 +1,10 @@
+/* $Id$
+ *
+ * Scrambler module.
+ *
+ * Copyright (C) 1998 Morten Rolland [Morten.Rolland@asker.mail.telia.com]
+ */
+
+#define CMD_SCRAMBLER_INIT 0x01
+
+int scrambler_construct(ifax_modp self, va_list args);

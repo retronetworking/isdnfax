43330800f64f7fb4aeaacbb17298bec79bec5d63
Morten Rolland <Morten.Rolland@asker.mail.telia.com>
1999-01-06 00:04:23 +0000

    Added more explicit signed/unsigned types for use in DSP code.

--- include/ifax/types.h
+++ include/ifax/types.h
@@ -6,14 +6,19 @@
 /* FIXME - We should autoconf that !
  */
 
+#ifndef _IFAX_TYPES_H
+#define _IFAX_TYPES_H
+
 typedef unsigned char ifax_uint8;
-typedef signed   char ifax_int8;
+typedef signed   char ifax_sint8;
+typedef          char ifax_int8;
 
 typedef unsigned short ifax_uint16;
-typedef short ifax_int16;
+typedef signed   short ifax_sint16;
+typedef          short ifax_int16;
 
 typedef unsigned int ifax_uint32;
-typedef int ifax_int32;
+typedef signed   int ifax_sint32;
+typedef          int ifax_int32;
 
-//typedef unsigned long long ifax_uint64;
-//typedef long long ifax_int64;
+#endif

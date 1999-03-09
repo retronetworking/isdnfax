805cf222caf2ae159d1eef75c74605ab5e923b0e
Morten Rolland <Morten.Rolland@asker.mail.telia.com>
1999-03-09 20:33:54 +0000

    Simple cleanup.

--- misc/watchdog.c
+++ misc/watchdog.c
@@ -26,9 +26,14 @@
 ******************************************************************************
 */
 
+/* Set up a signal-handeler for the 'profiling' signal: On Linux, this
+ * signal is called when a certain ammount of CPU-power has been used
+ * by the process.  It is used for profiling programs, but we use it
+ * to bail out in case of an infinite loop.  This is needed for real-
+ * time applications.
+ */
 
 #include <stdio.h>
-/*#include <signum.h>*/
 #include <signal.h>
 #include <sys/time.h>
 
@@ -42,7 +47,7 @@ static struct itimerval timer;
 
 static void watchdog_trigger(int signum)
 {
-  fprintf(stderr,"\n\n%s: Watchdog timer triggered a termination\n",progname);
+  fprintf(stderr,"\n\n%s: Watchdog timer caused termination\n",progname);
   exit(17);
 }
 
@@ -61,7 +66,7 @@ void reset_watchdog_timer(void)
   timer.it_interval.tv_sec = 0;
   timer.it_interval.tv_usec = 0;
   if ( setitimer(ITIMER_PROF, &timer, (struct itimerval *)0) < 0 ) {
-    fprintf(stderr,"%s: Can't install watchdog timer\n",progname);
+    fprintf(stderr,"%s: Can't reset watchdog timer\n",progname);
     exit(1);
   }
 }

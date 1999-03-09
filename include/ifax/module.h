ba3efe5f2db55e668bfab36b55a784b1b0d81522
Morten Rolland <Morten.Rolland@asker.mail.telia.com>
1999-03-09 20:54:26 +0000

    Added License banner and #ifndef/#endif for multiple inclusion.

--- include/ifax/module.h
+++ include/ifax/module.h
@@ -1,10 +1,36 @@
 /* $Id$
- * ISDNfax module layer
- * (c) 1998 Andreas Beck 	<becka@ggi-project.org>
- */
-   
+******************************************************************************
+
+   Fax program for ISDN.
+   Module layer.
+
+   Copyright (C) 1998 Andreas Beck      [becka@ggi-project.org]
+  
+   Permission is hereby granted, free of charge, to any person obtaining a
+   copy of this software and associated documentation files (the "Software"),
+   to deal in the Software without restriction, including without limitation
+   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+   and/or sell copies of the Software, and to permit persons to whom the
+   Software is furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+   THE AUTHOR(S) BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+   IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+******************************************************************************
+*/
+
 #include <stdlib.h>
 #include <stdarg.h>
+
+#ifndef _IFAX_MODULE_H
+#define _IFAX_MODULE_H
 
 /*
  * The ideas here were stolen from the EvStack concept, that was developed
@@ -102,3 +128,5 @@ void ifax_handle_demand(struct ifax_module *self, size_t len);
 /* Send a command to a module. Semantics are defined by the module.
  */
 int ifax_command(struct ifax_module *self, int command, ...);
+
+#endif
